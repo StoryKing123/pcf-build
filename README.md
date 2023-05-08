7bf1fa249ea6d8e74b76874dfb4270265b69da9d
BAMBOONETWORKS\steve.xuan <steve.xuan@bamboonetworks.com>
2023-05-08 16:58:13 +0800

    feat: add output param

---
+++ README.md
@@ -0,0 +1,8 @@
+
+This is to build pcf project without MSBuild
+
+# Build command
+pcf-build build -o package
+
+# dev
+pnpm link --global
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
 #!/usr/bin/env node
 import { Command } from "commander";
 import {
-  mkdirSync,
   ensureDirSync,
-  ensureFileSync,
   writeFileSync,
   readFileSync,
   readdirSync,
-  rename,
+  createWriteStream,
+  emptyDirSync,
 } from "fs-extra";
-import spawn from "cross-spawn";
-import { spawnSync } from "child_process";
 import xml2js, { parseString, parseStringPromise } from "xml2js";
-import path, { PlatformPath } from "path";
+import path from "path";
+import * as archive from "archiver";
 
 type Project = {
   name: string;
@@ -45,22 +43,18 @@ const initSolution = (prefix: string, name: string) => {
 program
   .command("build")
   .version("0.0.1")
-  .action(async (option, options) => {
+  .option("-o, --output <output>", "output dir")
+  .action(async (option) => {
+    // console.log(option);
+    const outputDir = option?.output ?? "package";
     let projects: Project[] = [];
-    // let projects;
     const solutionPath = process.cwd();
-
     const currentFolder = path.basename(process.cwd());
+    // const outputName = "package";
     const solutionXMLPath = path.resolve(
       process.cwd(),
       "src/Other/Solution.xml"
     );
-
-    // console.log(path.dirname(process.cwd()));
-
-    // console.log(path.basename(path.dirname(process.cwd())));
-    // console.log(process.cwd());
-
     const solutionCdsprojContent = readFileSync(
       path.join(process.cwd(), `${currentFolder}.cdsproj`),
       {
@@ -100,9 +94,6 @@ program
 
     const solutionXML = await parseStringPromise(solutionContent);
 
-    // console.log(solutionXML);
-    // console.log(solutionXML.ImportExportXml.SolutionManifest);
-
     const publisherName =
       solutionXML.ImportExportXml.SolutionManifest[0].Publisher[0]
         .UniqueName[0];
@@ -111,22 +102,70 @@ program
       solutionXML.ImportExportXml.SolutionManifest[0].Publisher[0]
         .CustomizationPrefix[0];
 
-    copyPackage(solutionPath);
-    assembleProject(projects, publisherName, publisherPrefix, solutionPath);
-    copyProjects(projects, publisherName, publisherPrefix, solutionPath);
+    clearDist(solutionPath, outputDir);
+
+    copyPackage(solutionPath, outputDir);
+    assembleProject(
+      projects,
+      publisherName,
+      publisherPrefix,
+      solutionPath,
+      outputDir
+    );
+    copyProjects(
+      projects,
+      publisherName,
+      publisherPrefix,
+      solutionPath,
+      outputDir
+    );
+    compress(solutionPath, outputDir);
   });
+
+function clearDist(solutionPath: string, outputDir: string) {
+  emptyDirSync(path.resolve(solutionPath, outputDir));
+}
+
+function compress(solutionPath: string, outputDir: string) {
+  const compressor = archive.create("zip", {});
+  compressor.directory(path.resolve(solutionPath, outputDir), false);
+  const output = createWriteStream(
+    path.resolve(solutionPath, outputDir, "Solution.zip")
+  );
+  compressor.pipe(output);
+  compressor.finalize();
+}
 
 function assembleProject(
   projects: Project[],
   publisherName: string,
   publisherPrefix: string,
-  solutionPath: string
+  solutionPath: string,
+  outputDir: string
 ) {
-  appendContentType(projects, publisherName, publisherPrefix, solutionPath);
+  appendContentType(
+    projects,
+    publisherName,
+    publisherPrefix,
+    solutionPath,
+    outputDir
+  );
 
-  appendSolution(projects, publisherName, publisherPrefix, solutionPath);
+  appendSolution(
+    projects,
+    publisherName,
+    publisherPrefix,
+    solutionPath,
+    outputDir
+  );
 
-  appendCustomization(projects, publisherName, publisherPrefix, solutionPath);
+  appendCustomization(
+    projects,
+    publisherName,
+    publisherPrefix,
+    solutionPath,
+    outputDir
+  );
 
   // projects.forEach((project) => {
   //content
@@ -136,11 +175,12 @@ function appendContentType(
   projects: Project[],
   publisherName: string,
   publisherPrefix: string,
-  solutionPath: string
+  solutionPath: string,
+  outputDir: string
 ) {
   const contentTypesFilePath = path.resolve(
     solutionPath,
-    "./package/[Content_Types].xml"
+    `./${outputDir}/[Content_Types].xml`
   );
 
   const overrideContent = projects
@@ -164,9 +204,13 @@ async function appendSolution(
   projects: Project[],
   publisherName: string,
   publisherPrefix: string,
-  solutionPath: string
+  solutionPath: string,
+  outputDir: string
 ) {
-  const solutionXMLPath = path.resolve(solutionPath, "./package/solution.xml");
+  const solutionXMLPath = path.resolve(
+    solutionPath,
+    `./${outputDir}/solution.xml`
+  );
   const solutionXMLContent = readFileSync(solutionXMLPath, "utf-8");
   const solutionXML = await parseStringPromise(solutionXMLContent);
   solutionXML.ImportExportXml.SolutionManifest[0].RootComponents = projects.map(
@@ -191,11 +235,12 @@ async function appendCustomization(
   projects: Project[],
   publisherName: string,
   publisherPrefix: string,
-  solutionPath: string
+  solutionPath: string,
+  outputDir: string
 ) {
   const customizationsXMLPath = path.resolve(
     solutionPath,
-    "./package/customizations.xml"
+    `./${outputDir}/customizations.xml`
   );
   const customizationsXMLContent = readFileSync(customizationsXMLPath, "utf-8");
   const customizationsXML = await parseStringPromise(customizationsXMLContent);
@@ -218,14 +263,14 @@ async function appendCustomization(
   // rename(cost)
 }
 
-function copyPackage(solutionPath: string) {
+function copyPackage(solutionPath: string, outputDir: string) {
   shell.cp(
     path.resolve(solutionPath, "./src/Other/Customizations.xml"),
-    path.resolve(solutionPath, "./package/customizations.xml")
+    path.resolve(solutionPath, `./${outputDir}/customizations.xml`)
   );
   shell.cp(
     path.resolve(solutionPath, "./src/Other/Solution.xml"),
-    path.resolve(solutionPath, "./package/solution.xml")
+    path.resolve(solutionPath, `./${outputDir}/solution.xml`)
   );
   // shell.cp(
   //   "-R",
@@ -238,15 +283,18 @@ function copyProjects(
   projects: Project[],
   publisherName: string,
   publisherPrefix: string,
-  solutionPath: string
+  solutionPath: string,
+  outputDir: string
 ) {
   projects.forEach((project) => {
     const wholeName = `${publisherPrefix}_${project.namespace}.${project.name}`;
-    ensureDirSync(path.resolve(solutionPath, `package/Controls/${wholeName}`));
+    ensureDirSync(
+      path.resolve(solutionPath, `${outputDir}/Controls/${wholeName}`)
+    );
     shell.cp(
       "-R",
       path.resolve(project.path, project.name, "*"),
-      path.resolve(solutionPath, `package/Controls/${wholeName}`)
+      path.resolve(solutionPath, `${outputDir}/Controls/${wholeName}`)
     );
   });
 }
@@ -257,13 +305,11 @@ async function runBuild(projectsPath: string[]) {
   const outputPaths = projectsPath.map(async (projectPath) => {
     const outputPath = path.join(projectPath, "./out/controls");
     ensureDirSync(outputPath);
-    // console.log(projectPath);
-    // console.log(outputPath);
-    // const buildRes = shell.exec(
-    //   `npm run build -- --noColor --buildMode development --outDir "${outputPath}
-    //   " --buildSource MSBuild`,
-    //   { cwd: projectPath }
-    // );
+    const buildRes = shell.exec(
+      `npm run build -- --noColor --buildMode development --outDir "${outputPath}
+      " --buildSource MSBuild`,
+      { cwd: projectPath }
+    );
     const project = readdirSync(outputPath);
 
     const projectControlManifestXMLContent = readFileSync(
@@ -273,9 +319,6 @@ async function runBuild(projectsPath: string[]) {
     const projectControlManifestXML = await parseStringPromise(
       projectControlManifestXMLContent
     );
-    // console.log(projectControlManifestXML);
-    // console.log(projectControlManifestXML.manifest);
-    console.log(projectControlManifestXML.manifest.control);
     const namespace =
       projectControlManifestXML.manifest.control[0]["$"].namespace;
     return { name: project[0], path: outputPath, namespace };
@@ -283,143 +326,4 @@ async function runBuild(projectsPath: string[]) {
   return outputPaths;
   //todo read from config file?
 }
-
-program
-  // .description('命令描述') // 命令描述
-  .option("-d, --dir <dirname>", "带参选项描述", "选项默认值"); // 带参选项（选项全称 取值时转成驼峰写法），支持设置默认值
-
-program
-  .command("is")
-  .version("0.0.1")
-  .description("子命令描述") // 命令描述
-  .option("-pn,<publisherName>", "publisherName") // 参数，必填
-  .option("-pp,<publisherPrefix>", "publisherPrefix") // 参数，必填
-  /*
-          argument 与 option 共存时：参数1为argument，参数2为option；否则参数1是argument或option
-          添加多个argument时，依次为action的参数1、参数2、参数3
-      */
-  .action(async (option, options) => {
-    const publisherPrefix = option.Pp;
-    const publisherName = option.Pn;
-
-    const publisher = `${publisherPrefix}_${publisherName}`;
-    const project = "PCF";
-
-    const projects = readdirSync(path.resolve(process.cwd(), "./out/controls"));
-    console.log(projects);
-
-    // shell;
-    // const wholeName = `${publisher}.${project}`;
-    console.log("start");
-    ensureDirSync("solution");
-    // const child = spawn(
-    //   "pac",
-    //   //   [`solution init --publisher-name PCF --publisher-prefix PCF`],
-    //   //   "pac solution init --publisher-name PCF --publisher-prefix PCF",
-    //   ["init", "--publisher-name", "PCF", "--publisher-prefix", "PCF"],
-    //   { stdio: "inherit", cwd: path.resolve(process.cwd(), "./solution") }
-    // );
-    initSolution(publisherPrefix, publisherName);
-
-    console.log("create file");
-
-    const relationshipFile = path.resolve(
-      process.cwd(),
-      "./solution/package/[Content_Types].xml"
-    );
-    ensureFileSync(path.resolve(relationshipFile));
-
-    //todo
-    // writeFileSync(
-    //   relationshipFile,
-    //   `<?xml version="1.0" encoding="utf-8"?><Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types"><Default Extension="xml" ContentType="text/xml" /><Default Extension="js" ContentType="application/octet-stream" /><Override PartName="/Controls/${wholeName}/ControlManifest.xml" ContentType="application/octet-stream" /></Types>`
-    // );
-
-    // ensureDirSync(
-    //   path.resolve(process.cwd(), `./solution/package/Controls/${wholeName}`)
-    // );
-    console.log("copy build output");
-
-    projects.forEach((project) => {
-      const wholeName = `${publisher}.${project}`;
-      ensureDirSync(
-        path.resolve(process.cwd(), `./solution/package/Controls/${wholeName}`)
-      );
-      shell.cp(
-        "-R",
-        path.resolve(process.cwd(), `./out/controls/${project}`),
-        path.resolve(process.cwd(), `./solution/package/Controls/${wholeName}`)
-      );
-    });
-
-    shell.cp(
-      "-R",
-      path.resolve(process.cwd(), "./solution/src/Other/*"),
-      path.resolve("./solution/package")
-    );
-
-    //modify solution xml
-
-    const solutionPath = path.resolve(
-      process.cwd(),
-      "./solution/package/solution.xml"
-    );
-    let data = readFileSync(solutionPath, { encoding: "utf-8", flag: "r" });
-    // console.log(data);
-    const xml = await parseStringPromise(data);
-    console.log("show xml");
-
-    // console.log(xml.ImportExportXml.SolutionManifest[0].RootComponents);
-
-    xml.ImportExportXml.SolutionManifest[0].RootComponents = [];
-    projects.forEach((project) => {
-      const wholeName = `${publisher}.${project}`;
-      xml.ImportExportXml.SolutionManifest[0].RootComponents.push({
-        RootComponents: {
-          $: {
-            type: 66,
-            schemaName: wholeName,
-            behavior: "0",
-          },
-        },
-      });
-    });
-
-    const builder = new xml2js.Builder();
-    const modifiedXmlString = builder.buildObject(xml);
-    writeFileSync(solutionPath, modifiedXmlString);
-    // console.log(modifiedXmlString);
-
-    const customizationsPath = path.resolve(
-      process.cwd(),
-      "./solution/package/Customizations.xml"
-    );
-    const customizationsXmlString = readFileSync(customizationsPath, {
-      encoding: "utf-8",
-      flag: "r",
-    });
-    const customizationsXml = await parseStringPromise(customizationsXmlString);
-    customizationsXml.ImportExportXml.CustomControls = [];
-    projects.forEach((project) => {
-      const wholeName = `${publisher}.${project}`;
-
-      customizationsXml.ImportExportXml.CustomControls.push({
-        CustomControl: {
-          Name: wholeName,
-          FileName: `/Controls/${wholeName}/ControlManifest.xml`,
-        },
-      });
-    });
-
-    // const builder = new xml2js.Builder();
-    const modifiedCustomizationXmlString =
-      builder.buildObject(customizationsXml);
-    console.log("show customzation");
-
-    // console.log(modifiedCustomizationXmlString);
-    writeFileSync(customizationsPath, modifiedCustomizationXmlString);
-
-    //
-  });
-
 program.parse(process.argv);
